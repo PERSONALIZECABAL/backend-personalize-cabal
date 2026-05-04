a42a4c5643ef75a10b482b85c63154e4b321ae03
PERSONALIZECABAL <cabalbelize@gmail.com>
2026-05-04 17:25:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
-const mercadopago = require("mercadopago");
+const { MercadoPagoConfig, Payment } = require('mercadopago');
 
-mercadopago.configure({
+const client = new MercadoPagoConfig({
     APP_USR-fa7a2beb-7616-44dd-85b6-d685d2ecb259: process.env.TOKEN
 });
 
 app.post("/pedido", async (req, res) => {
 
-    const { itens, total } = req.body;
-
     try {
 
-        const pagamento = await mercadopago.payment.create({
-            transaction_amount: Number(total),
-            description: "Pedido PERSONALIZE CABAL",
-            payment_method_id: "pix",
-            payer: {
-                email: "pablo.lancabal@hotmail.com"
+        const payment = new Payment(client);
+
+        const result = await payment.create({
+            body: {
+                transaction_amount: 10,
+                description: "Pedido PERSONALIZE CABAL",
+                payment_method_id: "pix",
+                payer: {
+                    email: "pablo.lancabal@hotmail.com"
+                }
             }
         });
 
-        const pix = pagamento.body.point_of_interaction.transaction_data;
+        const dados = result.point_of_interaction.transaction_data;
 
         res.json({
-            qr_code: pix.qr_code,
-            qr_code_base64: pix.qr_code_base64
+            qr_code: dados.qr_code,
+            qr_code_base64: dados.qr_code_base64
         });
 
     } catch (error) {
-        console.log(error);
+        console.log("ERRO REAL:", error);
         res.status(500).send("Erro ao gerar Pix");
     }
 });
